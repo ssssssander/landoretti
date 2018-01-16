082792f1f19e17331a107222cf522c679a57c717
Sander Borret <sander.borret@student.kdg.be>
2018-01-17 00:53:23 +0100

    add auction_cards includes

--- resources/views/art.blade.php
+++ resources/views/art.blade.php
@@ -3,25 +3,5 @@
 @section('title', trans('art.art'))
 
 @section('content')
-    {!! $auctions->appends($request->except(['page']))->links() !!}
-    <div class="auction-cards">
-        @foreach($auctions as $auction)
-            <div class="auction-card">
-                <a href="{{ route('auctionDetail', ['auction' => $auction, 'auctionTitle' => clean($auction->title)]) }}">
-                    <img src="{{ asset("storage/{$auction->artwork_image_path}") }}" alt="{{ $auction->title }}">
-                </a>
-                <div class="auction-info">
-                    <div class="year">{{ $auction->year }}, {{ $auction->artist }}</div>
-                    <div class="title">
-                        <a href="{{ route('auctionDetail', ['auction' => $auction, 'auctionTitle' => clean($auction->title)]) }}">{{ $auction->title }}</a>
-                    </div>
-                    <div class="price">€ {{ formatPrice($auction->min_price) }}</div>
-                    <span class="remaining-time" data-end-date="{{ $auction->end_date }}"></span>
-                    <a href="{{ route('auctionDetail', ['auction' => $auction, 'auctionTitle' => clean($auction->title)]) }}" class="small-button">@lang('art.visit_auction')</a>
-                </div>
-            </div>
-        @endforeach
-    </div>
-    {!! $auctions->appends($request->except(['page']))->links() !!}
-    @include('includes.scripts.remaining_time')
+    @include('includes.auction_cards', ['auctions' => $auctions])
 @endsection
